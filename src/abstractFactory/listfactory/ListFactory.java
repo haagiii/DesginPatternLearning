3a7c32544d0f44462b85156e98fe5c326f07384b
haagiii <hagwiara.kouji@gmail.com>
2013-03-11 21:04:25 +0900

    Chap.8 Abstract Factory

---
+++ src/abstractFactory/listfactory/ListFactory.java
@@ -0,0 +1,21 @@
+package abstractFactory.listfactory;
+
+import abstractFactory.factory.Factory;
+import abstractFactory.factory.Link;
+import abstractFactory.factory.Page;
+import abstractFactory.factory.Tray;
+
+public class ListFactory extends Factory{
+	public Link createLink(String caption, String url){
+		return new ListLink(caption, url);
+	}
+
+	public Tray createTray(String caption){
+		return new ListTray(caption);
+	}
+
+	public Page createPage(String title, String author){
+		return new ListPage(title, author);
+	}
+}
+
